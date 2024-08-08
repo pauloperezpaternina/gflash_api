6c7a331eea727f3ad1e71940c0c6d00ec0295b5e
Paulo Pérez <pauloperez@bittronic.net>
2024-08-07 22:18:15 -0500

    update README and added history chat

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-"# gflash_api" 
+# gflash_api
+
+Python routine to consume Google Flash 1.5 API or Google AI models
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/pauloperezpaternina/gflash_api.git
+```
+
+Go to the project directory
+
+```bash
+  cd gflash_api
+```
+
+Install dependencies
+
+```bash
+ pip install -r requirements.txt
+```
+
+Run
+
+```bash
+  python main.py
+```
+
+
+## Author
+
+- [@pauloperezpaternina](https://www.github.com/pauloperezpaternina)
+
+
+## Badges
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
+
--- main.py
+++ main.py
@@ -2,14 +2,39 @@ import google.generativeai as genai
 
 import os
 from dotenv import load_dotenv
+
 # os.environ.get('GOOGLE_API_KEY') without load_dotenv
 load_dotenv()
 
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
+"""
+Models
+models/gemini-1.5-flash
+models/gemini-1.5-pro-latest
+models/gemini-1.5-pro
+models/gemini-1.0-pro
+
+"""
 version = 'models/gemini-1.5-flash'
-model = genai.GenerativeModel(version)
-prompt = "Quien eres, responde detalladamente"
-response = model.generate_content(prompt)
-print(response.text)
+try:
+    model = genai.GenerativeModel(version)
+    prompt = "quien gano el ultimo mundial de futbol"
+    response = model.generate_content(prompt)
+    print(response.text)
+except Exception as err:
+    print(err)
+
+#Chat Mode
+print("Chat mode")
+messages_list =[]
+chat = model.start_chat(history=messages_list)
+question1 = chat.send_message("Hola, a que distancia esta el sol?").text
+question2 = chat.send_message("puedes crear palabras nuevas?").text
+
+
+#chat history
+print("Chat History:")
+for message in chat.history:
+    print(message)
